e329a0608fa5e2d6035233045bd2bdf6883274b6
loureiro-lucas <lucas-loureiro@outlook.com>
2022-02-06 13:37:25 -0300

    fix requirement 7 again again againnnn

--- desafio7.sql
+++ desafio7.sql
@@ -2,7 +2,7 @@ SELECT
 	art.nome AS artista,
 	alb.titulo AS album,
 	COUNT(u_a.artista_id) AS seguidores
-FROM SpotifyClone.artista AS art
+FROM SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.albuns AS alb
 	ON alb.artista_id = art.id
 INNER JOIN SpotifyClone.usuario_artista AS u_a
