128b8878eeb7fbd64719bd5de7bca61cde50af5a
loureiro-lucas <lucas-loureiro@outlook.com>
2022-02-06 11:56:06 -0300

    fix requirement 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	ROUND(MIN(p.valor), 2) AS faturamento_minimo,
-  MAX(p.valor) AS faturamento_maximo,
-  ROUND(AVG(p.valor), 2) AS faturamento_medio,
-  ROUND(SUM(p.valor), 2) AS faturamento_total
+	CONCAT(MIN(p.valor), '.00') AS faturamento_minimo,
+  CONVERT(MAX(p.valor), CHAR) AS faturamento_maximo,
+  CONVERT(ROUND(AVG(p.valor), 2), CHAR) AS faturamento_medio,
+  CONVERT(ROUND(SUM(p.valor), 2), CHAR) AS faturamento_total
 FROM SpotifyClone.planos AS p
 INNER JOIN SpotifyClone.usuarios AS u
 	ON u.plano_id = p.id;
